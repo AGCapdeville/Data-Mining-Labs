c00cceb8e55bfd4d6905368f9cc69aa3e9af0c44
Adam Capdeville <agcapdeville@gmail.com>
2020-03-13 21:17:30 -0700

    Lab 7 done

--- Lab7.py
+++ Lab7.py
@@ -5,35 +5,58 @@ Lab 7
 print ("Lab 7")
 
 import numpy as np
-import pandas as pd
 
 ##########Part 0 ###########
+
 '''
     1)  load iris-data-1.csv using Pandas and save it as a dataframe (data)
     preprocess your data: 
         a) remove repeated samples (Hint: check "ID")
         b) handle missing data in "color" and preprocess the values for this feature
+        
 '''
 # YOUR CODE GOES HERE
-with open("./data/iris-data-color.csv") as csvfile:
+import pandas as pd
+
+with open('./data/iris-data-color.csv') as csvfile:
     data = pd.read_csv(csvfile, delimiter=',')
-print(data)
+modifiedData = data.drop_duplicates("ID", keep = 'first')
+
+modifiedData = modifiedData.replace(["Blue", "Bluee"], "blue")
+modifiedData = modifiedData.replace("Red", "red")
+modifiedData = modifiedData.replace(["None", "Nan", "U", "none"], float('nan'))
+
+modifiedData.dropna(inplace = True)
+modifiedData = modifiedData.reset_index(drop = True)
 
 ##########Part 1 ###########
+
 '''
     1)  train a KKN classifier for your data  (K = 11). Features: sepal_length,sepal_width,petal_length,petal_width, color
     Hint: pandas.DataFrame.drop() to drop unwanted features
 '''
+
 # YOUR CODE GOES HERE
 
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors = 11)
 
+modifiedData = modifiedData.drop(columns = ["ID"])
 
+modifiedData2 = modifiedData
+modifiedDataOHE = modifiedData
+modifiedDataLE = modifiedData
 '''
     2)  what is the error in previous part? how you can fix it?
     fix the error and train your model.
     Hint: pandas.get_dummies()
 '''
 # YOUR CODE GOES HERE
+
+colors = pd.get_dummies(modifiedData['color'])
+modifiedData = pd.concat([modifiedData, colors], axis=1)
+
+neigh.fit(modifiedData[["sepal_length","sepal_width","petal_length","petal_width", "blue", "pink", "purple", "red"]], modifiedData["species"])
 
 '''
     3)  test your trained model and evaluate the result for given test set.
@@ -43,16 +66,102 @@ X_test = np.asarray([[5 , 1, 0.2 , 5,'red'],[0.9 , 7, 6.2 , 2.1,'red'], [0.9 , 7
 Y_test = np.asarray(['virginica', 'virginica','virginica', 'versicolor' ,'setosa'])
 # YOUR CODE GOES HERE
 
+df = pd.DataFrame(data = X_test)
+colors = pd.get_dummies(df[4])
+df = pd.concat([df, colors], axis=1)
+df = df.drop(columns = [4])
+
+X_test = np.asarray(df)
+
+from sklearn.metrics import classification_report
+
+prediction = (neigh.predict(X_test))
+report = classification_report(Y_test, prediction)
+#print(report)
+
 '''
     4)  use DictVectorizer from sklearn.feature_extraction to solve Q2
 '''
 # YOUR CODE GOES HERE
 
+from sklearn.feature_extraction import DictVectorizer
+
+
+# myDict = (modifiedData2[['color']]).to_dict('records')
+myDict = (modifiedData2[['color']]).to_dict('records')
+# print(myDict)
+
+vec = DictVectorizer()
+x = vec.fit_transform(myDict).toarray()
+# print(x)
+# print(vec.get_feature_names())
+colors = pd.DataFrame.from_dict(x)
+feature = vec.get_feature_names()
+for i in range(len(feature)):
+    feature[i] = feature[i][6:len(feature[i])]
+# print(feature)
+colors.columns = feature
+# print(colors)
+
+modifiedData2 = pd.concat([modifiedData2, colors], axis=1)
+neigh.fit(modifiedData[["sepal_length","sepal_width","petal_length","petal_width", "blue", "pink", "purple", "red"]], modifiedData["species"])
 
 '''
     5)  use OneHotEncoder and LabelEncoder from sklearn.preprocessing to solve Q2 and Q3
 '''
 # YOUR CODE GOES HERE
+
+# YOUR CODE GOES HERE
+from sklearn.preprocessing import OneHotEncoder
+
+encoder = OneHotEncoder()
+data = encoder.fit_transform(modifiedDataOHE[['color']]).toarray()
+
+modifiedDataOHE = np.concatenate([modifiedDataOHE, data], axis=1)
+# colors = encoder.get_feature_names(['color'])
+colors = encoder.get_feature_names(['color'])
+df_ohe = pd.DataFrame(data = modifiedDataOHE)
+
+print(colors[0])
+
+df_ohe.columns = ["sepal_length","sepal_width","petal_length","petal_width", "species", "color", "blue", "pink", "purple", "red"]
+# prints out the label for encoder
+print(df_ohe)
+
+neigh.fit(modifiedData[["sepal_length","sepal_width","petal_length","petal_width", "blue", "pink", "purple", "red"]], modifiedData["species"])
+# prints out in terms of numpy, but lose the labels for each column 
+# print(df_ohe)
+prediction = (neigh.predict(X_test))
+report = classification_report(Y_test, prediction)
+print(report)
+
+from sklearn.preprocessing import LabelEncoder
+
+label_enc = LabelEncoder()
+data = label_enc.fit_transform(modifiedDataLE[['color']])
+le = pd.DataFrame({'color_label':data})
+modifiedDataLE = pd.concat([modifiedDataLE, le], axis = 1)
+
+
+X_test = np.asarray([[5 , 1, 0.2 , 5,'red'],[0.9 , 7, 6.2 , 2.1,'red'], [0.9 , 7, 6.2 , 2.1,'pink'] , [1.9 , 4, 5 , 0.1,'purple'], [5.9 , 3.3, 0.2 , 2.7,'blue']])
+Y_test = np.asarray(['virginica', 'virginica','virginica', 'versicolor' ,'setosa'])
+
+
+testDF = pd.DataFrame(data = X_test)
+colors = label_enc.fit_transform(testDF[4])
+colorsDF = pd.DataFrame(data = colors)
+testDF = pd.concat([testDF, colorsDF], axis = 1)
+testDF = testDF.drop(columns = [4])
+X_test = np.asarray(testDF)
+
+neigh.fit(modifiedDataLE[["sepal_length","sepal_width","petal_length","petal_width", "color_label"]], modifiedDataLE["species"])
+
+prediction = (neigh.predict(X_test))
+report = classification_report(Y_test, prediction)
+print(report)
+
+
+
 
 ##########Part 2 ###########
 
